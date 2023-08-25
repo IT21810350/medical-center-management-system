9d9ac19f17cc1a0fb4828222af7fc1c161c64a76
ravindukavishka <ravindukavishka841@gmail.com>
2023-08-26 01:06:51 +0530

    Edit table

--- client/src/views/hr-views/hr-home.js
+++ client/src/views/hr-views/hr-home.js
@@ -11,6 +11,13 @@ import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
 import { PieChart, pieArcClasses } from '@mui/x-charts/PieChart';
 import Grid from '@mui/material/Grid';
+// import Button from '@mui/material/Button';
+
+// import CssBaseline from '@mui/material/CssBaseline';
+// import AppBar from '@mui/material/AppBar';
+// import Toolbar from '@mui/material/Toolbar';
+// import Link from '@mui/material/Link';
+
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -62,49 +69,49 @@ const data = [
     createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-const NAV = () => {
+// const NAV = () => {
 
-    return (
-        <Box sx={{ display: "flex" }}>
-            <CssBaseline />
-            <AppBar position="fixed">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Logo
-                    </Typography>
-                    <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
-                        Home
-                    </Button>
-                    <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
-                        Contact Us
-                    </Button>
-                    <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
-                        About Us
-                    </Button>
-                    <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
-                        Lab Facilities
-                    </Button>
-                    <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
-                        Popular Doctors
-                    </Button>
-                    <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
-                        HR Module
-                    </Button>
-                    <Link color="inherit" to="/signup">
-                        <Button variant="contained" color="info">
-                            Signup
-                        </Button>
-                    </Link>
-                    {/* <Link color="inherit" href="#" onClick={handleLogout}>
-          <Button variant="contained" color="warning">
-            Logout
-          </Button>
-        </Link> */}
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
-}
+//     return (
+//         <Box sx={{ display: "flex" }}>
+//             <CssBaseline />
+//             <AppBar position="fixed">
+//                 <Toolbar>
+//                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//                         Logo
+//                     </Typography>
+//                     <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
+//                         Home
+//                     </Button>
+//                     <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
+//                         Contact Us
+//                     </Button>
+//                     <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
+//                         About Us
+//                     </Button>
+//                     <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
+//                         Lab Facilities
+//                     </Button>
+//                     <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
+//                         Popular Doctors
+//                     </Button>
+//                     <Button color="inherit" href="#" variant="outlined" sx={{ mr: 2 }}>
+//                         HR Module
+//                     </Button>
+//                     <Link color="inherit" to="/signup">
+//                         <Button variant="contained" color="info">
+//                             Signup
+//                         </Button>
+//                     </Link>
+//                     {/* <Link color="inherit" href="#" onClick={handleLogout}>
+//           <Button variant="contained" color="warning">
+//             Logout
+//           </Button>
+//         </Link> */}
+//                 </Toolbar>
+//             </AppBar>
+//         </Box>
+//     );
+// }
 
 const HR = () => {
   return (
@@ -197,77 +204,7 @@ const HR = () => {
 
 
   );
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <NAV />
-            <Grid container spacing={2}>
-
-                <Grid item xs={3}>
-                    <Item>
-                        <Card sx={{ maxWidth: 345 }}>
-                            <CardActionArea>
-                                <CardMedia
-                                    component="img"
-                                    height="200"
-                                    image={img1}
-                                    alt="green iguana"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Lizard
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                                        species, ranging across all continents except Antarctica
-                                    </Typography>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Share
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Item>
-                </Grid>
-
-                <Grid item xs={9}>
-                    <Item>
-                        <TableContainer component={Paper}>
-                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Dessert (100g serving)</TableCell>
-                                        <TableCell align="right">Calories</TableCell>
-                                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {rows.map((row) => (
-                                        <TableRow
-                                            key={row.name}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell component="th" scope="row">
-                                                {row.name}
-                                            </TableCell>
-                                            <TableCell align="right">{row.calories}</TableCell>
-                                            <TableCell align="right">{row.fat}</TableCell>
-                                            <TableCell align="right">{row.carbs}</TableCell>
-                                            <TableCell align="right">{row.protein}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Item>
-                </Grid>
-
-            </Grid>
-        </Box>
-    );
+   
 };
 
 export default HR;
