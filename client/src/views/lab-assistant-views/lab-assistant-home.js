22c4896e334c12b79ef1d3bad7b4fb71c670d814
it21803420 <it21803420@my.sliit.lk>
2023-08-26 01:09:25 +0530

    lab assistant

--- client/src/views/lab-assistant-views/lab-assistant-home.js
+++ client/src/views/lab-assistant-views/lab-assistant-home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LA = () => {
     return (
         <div>
-            <h2>Lab Assistant Home Page</h2>
+            <h2>Lab Assistant Home</h2>
         </div>
     );
 };
