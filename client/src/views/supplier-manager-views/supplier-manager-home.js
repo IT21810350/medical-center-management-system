cc56032a7522667635f1aad0d71eb09b3af8b111
S.Nallaperuma <it21306822@my.sliit.lk>
2023-09-01 21:54:06 +0530

    Supplier home page modify

--- client/src/views/supplier-manager-views/supplier-manager-home.js
+++ client/src/views/supplier-manager-views/supplier-manager-home.js
@@ -1,12 +1,63 @@
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
-const SM = () => {
-    return (
-        <div>
-        
-  
-        </div>
-    );
-};
 
-export default SM;
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function App() {
+  return (
+    <div>
+      
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Item>
+              <Card sx={{ maxWidth: 550 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="240"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt=""
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Ms.Santhushie Nallaperuma
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                       Supplier Manager
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Item>
+          </Grid>
+          
+          
+          
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            
+          </Grid>
+         
+        </Grid>
+      </Box>
+    </div>
+  );
+}
