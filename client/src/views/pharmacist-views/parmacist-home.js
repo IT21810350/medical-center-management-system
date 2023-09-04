1aeaf28628ed084aa78c294f6333448e9768649d
IT21809538 <it21809538@my.sliit.lk>
2023-09-04 14:42:11 +0530

    test

--- client/src/views/pharmacist-views/parmacist-home.js
+++ client/src/views/pharmacist-views/parmacist-home.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import img1 from '../../assets/img/supplier/sage.jpg';
 
-const Pharmacist = () => {
+const Pharmacist  = () => {
     return (
         <div>
-            <h2>Hello, I'm himaya uwangi</h2>
+            <h1>Welcome to our Pharmacy !</h1>
+            <h2>We'll always make time for you.. </h2>
         </div>
     );
 };
 
 export default Pharmacist;
+
+
