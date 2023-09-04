d2d8da1a57a4e70baa441e0ebc5d3cb5a89376b5
S.Nallaperuma <it21306822@my.sliit.lk>
2023-09-04 10:51:51 +0530

    Supplier registration form create

--- client/src/views/supplier-manager-views/supplier-registration.js
+++ client/src/views/supplier-manager-views/supplier-registration.js
@@ -1,68 +1,93 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
 
-export default function FormPropsTextFields() {
+
+export default function RegistrationForm() {
   return (
-    <Box
-      component="form"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: 2,
+    <Container
+      maxWidth="xl"
+      style={{
+        height: '100vh', 
+        width: '100vw',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundColor: 'rgba(155, 155, 155, 0.5)', // Adjust the alpha value to control transparency
       }}
-      noValidate
-      autoComplete="off"
     >
-      <h1>Supplier Registration</h1>
-      <div style={{ display: 'flex', gap: '20px' }}>
-        <div>
-          <TextField
-            required
-            id="outlined-required-first-name"
-            label="First Name"
-          />
-          <TextField
-            required
-            id="outlined-required-last-name"
-            label="Last Name"
-          />
-          <TextField
-            required
-            id="outlined-required-email"
-            label="Email Address"
-            type="email"
-          />
-        </div>
-        <div>
-          <TextField
-            required
-            id="outlined-required-address"
-            label="Address"
-          />
-          <TextField
-            required
-            id="outlined-required-phone"
-            label="Phone Numbers"
-          />
-          <TextField
-            required
-            id="outlined-required-nic"
-            label="NIC Number"
-          />
-        </div>
+      <div style={{ padding: '20px' }}>
+        <h1 style={{ textAlign: 'center' }}>Supplier Registration</h1>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <TextField
+              required
+              fullWidth
+              id="outlined-required-first-name"
+              label="First Name"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              fullWidth
+              id="outlined-required-last-name"
+              label="Last Name"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="outlined-required-email"
+              label="Email Address"
+              type="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="outlined-required-address"
+              label="Address"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              fullWidth
+              id="outlined-required-phone"
+              label="Phone Numbers"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              required
+              fullWidth
+              id="outlined-required-nic"
+              label="NIC Number"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="outlined-required-bio"
+              label="Your Bio"
+              multiline
+              rows={4}
+            />
+          </Grid>
+        </Grid>
       </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required-bio"
-          label="Your Bio"
-          multiline
-          rows={4}
-          fullWidth
-        />
-      </div>
-    </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+        <Button variant="contained" color="primary">
+          Submit
+        </Button>
+      </Box>
+    </Container>
   );
 }
