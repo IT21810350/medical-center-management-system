299f9ea71cb5ee4a450d4baff95f6e882de05da7
IT21809538 <it21809538@my.sliit.lk>
2023-09-15 17:37:30 +0530

    pharmacist changes

--- client/src/views/pharmacist-views/parmacist-home.js
+++ client/src/views/pharmacist-views/parmacist-home.js
@@ -8,12 +8,64 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import img1 from '../../assets/img/supplier/sage.jpg';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+
+
 
 const Pharmacist  = () => {
     return (
         <div>
-            <h1>Welcome to our Pharmacy !</h1>
-            <h2>We'll always make time for you.. </h2>
+            <Paper
+      sx={{
+        position: 'relative',
+        backgroundColor: 'grey.800',
+        color: '#fff',
+        mb: 4,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        //backgroundImage: `url(${post.image})`,
+      }}
+    >
+      {/* Increase the priority of the hero background image */}
+      {<img style={{ display: 'none' }}  />}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: 'rgba(201, 76, 76, 0.3)',
+        }}
+      />
+      
+      
+      <Grid container>
+        <Grid item md={12}>
+          <Box
+            sx={{
+              position: 'relative',
+              p: { xs: 3, md: 6 },
+              pr: { md: 0 },
+            }}
+          >
+            <Typography component="h1" variant="h3" color="inherit" align="center" gutterBottom>
+              Welcome to Our Pharmacy !
+            </Typography>
+            <Typography variant="h5" color="inherit" align="center" paragraph>
+              We'll always make time for you..
+            </Typography>
+            <Link variant="subtitle1" href="#">
+              
+              
+            </Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+            
         </div>
     );
 };
