0ef02d438484469b219ce2c6374d60284329fc56
S.Nallaperuma <it21306822@my.sliit.lk>
2023-09-21 04:49:31 +0530

    add supplier implement

--- client/src/views/financial-manager-views/financial-manager-home.js
+++ client/src/views/financial-manager-views/financial-manager-home.js
@@ -3,7 +3,24 @@ import React from 'react';
 const FM = () => {
     return (
         <div>
-            <h2>Hello, I'm Financial Manager</h2>
+            <section style={{backgroundImage: `url(${bg})`}}>
+        <header>
+        <img
+            src={menu}
+            className="menu-btn"
+            onClick={handleMenuClick}
+            alt="menu"
+        />
+        <h3 className="logo">Logo</h3>
+        </header>
+        <div className="center-vertical">
+        <div className="content">
+            <h1>Data Analytics</h1>
+            <p className="hero-subtitle">Making sense of your traffic.</p>
+            <a href="/" className="cta">Go to my stats</a>
+        </div>
+        </div>
+    </section>
         </div>
     );
 };
