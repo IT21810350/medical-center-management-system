d717a36d57884de95a57c2b7457faebc9990076f
IT21803420 <it21803420@my.sliit.lk>
2023-09-24 00:36:05 +0530

    fixing my mistakes becaus malshan told me to

--- backend/Routes/LabAssistantRoute.js
+++ backend/Routes/LabAssistantRoute.js
@@ -1,36 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-// CONTROLLERS 
-const {SampleController} = require("../Controllers/LabAssistantController");
-const {EquipmentController} = require("../Controllers/LabAssistantController");
-const {ReportController} = require("../Controllers/LabAssistantController");
-const {TestController} = require("../Controllers/LabAssistantController");
-
 // CRUD FUNCTION CONTROLLERS for each Model <- All CRUD Operations for each type exist here
-const {CreateSampleController} = require("../Controllers/LabAssistantController");
-const {getAllSampleController} = require("../Controllers/LabAssistantController");
-const {getSampleByIdController} = require("../Controllers/LabAssistantController");
-const {updateSampleByIdController} = require("../Controllers/LabAssistantController");
-const {deleteSampleByIdController} = require("../Controllers/LabAssistantController");
-
-const {createEquipmentController} = require("../Controllers/LabAssistantController");
-const {getAllEquipmentController} = require("../Controllers/LabAssistantController");
-const {getEquipmentByIdController} = require("../Controllers/LabAssistantController");
-const {updateEquipmentByIdController} = require("../Controllers/LabAssistantController");
-const {deleteEquipmentByIdController} = require("../Controllers/LabAssistantController");
-
-const {createReportController} = require("../Controllers/LabAssistantController");
-const {getAllReportController} = require("../Controllers/LabAssistantController");
-const {getReportByIdController} = require("../Controllers/LabAssistantController");
-const {updateReportByIdController} = require("../Controllers/LabAssistantController");
-const {deleteReportByIdController} = require("../Controllers/LabAssistantController");
-
-const {createTestController} = require("../Controllers/LabAssistantController");
-const {getAllTestController} = require("../Controllers/LabAssistantController");
-const {getTestByIdController} = require("../Controllers/LabAssistantController");
-const {updateTestByIdController} = require("../Controllers/LabAssistantController");
-const {deleteTestByIdController} = require("../Controllers/LabAssistantController");
+const {
+    CreateSampleController,
+    getAllSampleController,
+    getSampleByIdController,
+    updateSampleByIdController,
+    deleteSampleByIdController,
+    createEquipmentController,
+    getAllEquipmentController,
+    getEquipmentByIdController,
+    updateEquipmentByIdController,
+    deleteEquipmentByIdController,
+    createReportController,
+    getAllReportController,
+    getReportByIdController,
+    updateReportByIdController,
+    deleteReportByIdController,
+    createTestController,
+    getAllTestController,
+    getTestByIdController,
+    updateTestByIdController,
+    deleteTestByIdController
+} = require("../Controllers/LabAssistantController"); 
 
 
 router.post("/lab-test", TestController);
